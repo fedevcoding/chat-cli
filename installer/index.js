35509143f37dd574c6c80f36a22bf8c80863b8d3
Federico <federicocavallini100@gmail.com>
2023-11-26 10:45:29 +0100

    organize folder and add installer server

---
+++ installer/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/download', (req, res) => {
+    const filePath = path.join(__dirname, './inno-setup/Output/Chat cli.exe');
+    res.download(filePath, 'installer.exe');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
